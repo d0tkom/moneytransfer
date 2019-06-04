1029002a4c69054e78b7b82e383d3cb8066b9649
Patrik Singer <singerpatrik@gmail.com>
2019-06-04 14:51:40 +0100

    implement equal, hashcode, and tostring on models

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
     public final String id;
@@ -9,5 +10,26 @@ public class Account {
     public Account(String id, BigDecimal balance) {
         this.id = id;
         this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account that = (Account)o;
+        return Objects.equals(id, that.id) && Objects.equals(balance, that.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, balance);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", balance=" + balance +
+                "}";
     }
 }
--- src/main/java/model/Transfer.java
+++ src/main/java/model/Transfer.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Transfer {
     public final String id;
@@ -13,5 +14,31 @@ public class Transfer {
         this.source = source;
         this.target = target;
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transfer that = (Transfer)o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(source, that.source) &&
+                Objects.equals(target, that.target) &&
+                Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, source, target, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", source=" + source +
+                ", target=" + target +
+                ", amount=" + amount +
+                "}";
     }
 }
